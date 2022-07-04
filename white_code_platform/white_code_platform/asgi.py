56835bf3c98a4446b0d979dd1ab2031ac819287a
Yuri Kuznetsov <zipadump@gmail.com>
2022-07-04 14:25:04 +0300

    first-file deploy

---
+++ white_code_platform/white_code_platform/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for white_code_platform project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'white_code_platform.settings')
+
+application = get_asgi_application()
